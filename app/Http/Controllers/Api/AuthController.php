be8f40fb5f7d81863be3627f2f36b58c129cb19f
Kuldyaev <slavalion1@gmail.com>
2026-06-22 00:40:34 +0200

    first step api auth

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,18 +2,24 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Requests\Api\Auth\ApiLoginRequest;
+use App\Http\Requests\Api\Auth\ApiRegisterRequest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    public funcion register(ApiRegisterRequest $request): JsonResponse
+    public function register(ApiRegisterRequest $request): JsonResponse
     {
         $data = $request->validated();
 
         $user = User::query()->create($data);
 
-        $token = $user->createToken(name'api_token')->plainTextToken;
+        $token = $user->createToken(name:'api_token')->plainTextToken;
 
         return response()->json([
             'message' => 'User registered successfully',
@@ -41,7 +47,7 @@ class AuthController extends Controller
             ], 422);
         }
         
-        $token = $user->createToken(name'api_token')->plainTextToken;
+        $token = $user->createToken(name:'api_token')->plainTextToken;
 
         return response()->json([
             'message' => 'User logged in successfully',
@@ -61,7 +67,7 @@ class AuthController extends Controller
     {
        $user = $request->user();
        
-       ->currentAccessToken()->delete();
+       $user->currentAccessToken()->delete();
 
         return response()->json([
             'message' => 'User logged out successfully'
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,22 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('auth')->group(function (): void {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+    Route::middleware('auth:sanctum')->group(function (): void {
+        Route::get('me', [AuthController::class, 'me']);
+        Route::post('logout', [AuthController::class, 'logout']);
+    });
+});
+
+
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
